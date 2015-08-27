544d90c9ad4c16e682f846a9e1f7b7808a9a56c8
yu-gen <yu-gen@ubuntu.(none)>
2015-08-27 11:45:38 +0900

    十,餽,𢠽,慢の文字画像追加。50番台から番号づけ

---
+++ update.sql
@@ -0,0 +1,7 @@
+-- coding: utf-8
+-- 20150827
+-- 十 餽 𢠽 慢
+update kaiji_moji set img='089a-50' where id='w0540010';
+update kaiji_moji set img='222b-50' where id='w1800580';
+update kaiji_moji set img='509b-50' where id='w4081320';
+update kaiji_moji set img='509b-51' where id='w4081330';
