ee3a0e498f5adecd48cfc187d1b61557ea4ef239
yu-gen <yu-gen@ubuntu.(none)>
2015-09-04 06:34:24 +0900

    add那

--- update.sql
+++ update.sql
@@ -1,7 +1,13 @@
 -- coding: utf-8
--- 20150827
--- 十 餽 𢠽 慢
+-- images
+--   20150827: 十 餽 𢠽 慢
 update kaiji_moji set img='089a-50' where id='w0540010';
 update kaiji_moji set img='222b-50' where id='w1800580';
 update kaiji_moji set img='509b-50' where id='w4081320';
 update kaiji_moji set img='509b-51' where id='w4081330';
+--
+-- 異体字
+--   20150902: 禀
+update kaiji_moji set c='稟禀' where id='w1940020';
+--   20150904: 那
+update kaiji_moji set c='那𨙻' where id='w2291060';
